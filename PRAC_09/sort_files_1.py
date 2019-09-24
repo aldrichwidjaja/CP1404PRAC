5da0d96239de5f6ba9b6dc944fbb69fde39eed41
Aldrich Widjaja <50089724+aldrichwidjaja@users.noreply.github.com>
2019-09-24 17:15:28 +0800

    Add files via upload

---
+++ PRAC_09/sort_files_1.py
@@ -0,0 +1,16 @@
+import os
+def main():
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+        extension = filename.split('.')[-1]
+        print(extension)
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        print("{}/{}".format(extension, filename))
+        os.rename(filename, "{}/{}".format(extension, filename))
+
+main()
